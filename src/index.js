7d6cafdcc812505c4d68ec4f2304ac6295623068
Felipe300 <caracoloro@gmail.com>
2021-11-30 22:43:32 -0300

    Adding dotenv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import paymentRoutes from './routes/payment.routes'
+import 'dotenv/config.js'
 
 const app = express()
 const PORT = process.env.PORT || 5000
